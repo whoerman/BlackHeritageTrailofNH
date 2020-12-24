6d9b64873eae81f96ae0d8cc0226683d100d721d
Walter Hoerman <whoerman@gmail.com>
2020-12-23 20:28:43 -0500

    starting the database

---
+++ siteArray.js
@@ -0,0 +1,146 @@
+//array fo the sites
+let siteArray = [{
+        number: 1,
+        name: "Black Heritage Trail of New Hampshire office",
+        address: "222 Court Street",
+        comment: ""
+    },
+    {
+        number: 2,
+        name: "Governor John Langdon House",
+        address: "143 Pleasant Street",
+        comment: ""
+    },
+    {
+        number: 3,
+        name: "William Pitt Tavern",
+        address: "416 Court St.",
+        comment: "Strawbery Banke Museum"
+    },
+    {
+        number: 4,
+        name: "Docks and view of Portsmouth Naval Shipyard",
+        address: "Prescott Park",
+        comment: ""
+    },
+    {
+        number: 5,
+        name: "Sherburne House",
+        address: "Strawbery Banke campus",
+        comment: ""
+    },
+    {
+        number: 6,
+        name: "South Meeting House",
+        address: "280 Marcy Street",
+        comment: ""
+    },
+    {
+        number: 7,
+        name: "NH Gazette",
+        address: "corner of Howard and Washington Streets",
+        comment: "private"
+    },
+    {
+        number: 8,
+        name: "Cooper House and Beauty Shop",
+        address: "171 Washington Street",
+        comment: "private",
+    },
+    {
+        number: 9,
+        name: "Stoodley’s Tavern",
+        address: "14 Hancock Street",
+        comment: "Strawbery Banke Museum",
+    },
+    {
+        number: 10,
+        name: "Penhallow House",
+        address: "93 Washington Street",
+        comment: "Strawbery Banke Museum",
+    },
+    {
+        number: 11,
+        name: "Warner House",
+        address: "150 Daniel Street",
+        comment: "",
+    },
+    {
+        number: 12,
+        name: "Saint John’s Church",
+        address: "100 Chapel Street",
+        comment: "",
+    },
+    {
+        number: 13,
+        name: "Waterfront",
+        address: "Ceres Street",
+        comment: "",
+    },
+    {
+        number: 14,
+        name: "Moffatt-Ladd House",
+        address: "154 Market Street",
+        comment: "",
+    },
+    {
+        number: 15,
+        name: "Black Whipple House",
+        address: "127 High Street",
+        comment: "private",
+    },
+    {
+        number: 16,
+        name: "Meeting Rooms",
+        address: "14-16 Market Square",
+        comments: "corner of Daniel and Pleasant Streets",
+    },
+    {
+        number: 17,
+        name: "Town Pump and Stocks",
+        address: "next to North Church",
+        comment: "",
+    },
+    {
+        number: 18,
+        name: "North Church",
+        address: "Market Square",
+        comment: "",
+    },
+    {
+        number: 19,
+        name: "Pomp and Candace Spring House and Bakery",
+        address: "Church Street",
+        comment: "",
+    },
+    {
+        number: 20,
+        Name: "The Music Hall (The Temple )",
+        address: "Chestnut Street at Congress Street",
+        comment: "",
+    },
+    {
+        number: 21,
+        name: "John Paul Jones House",
+        address: "43 Middle Street",
+        comment: "",
+    },
+    {
+        number: 22,
+        name: "Rockingham House",
+        address: "401 State Street",
+        comment: "private",
+    },
+    {
+        number: 23,
+        name: "African Burying Ground",
+        address: "Chestnut Street",
+        comment: "between Court & State Streets",
+    },
+    {
+        number: 24,
+        name: "South Church",
+        address: "292 State Street",
+        comment: "",
+    }
+]
